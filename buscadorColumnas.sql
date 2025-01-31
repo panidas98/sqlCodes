3e51c6ff1a20b28692fd5c7678d34421d8b4b484
Juan Ochoa <132312199+panidas98@users.noreply.github.com>
2025-01-31 11:38:35 -0500

    Update buscadorColumnas.sql

--- buscadorColumnas.sql
+++ buscadorColumnas.sql
@@ -1,37 +1,50 @@
 -- Script para encontrar columnas específicas en todas las tablas de la base de datos
 -- Útil para mapear relaciones y explorar bases de datos sin documentación
 
--- Busca todas las tablas que contienen una columna con un nombre específico
-CREATE PROCEDURE sp_FindColumn
-    @ColumnName NVARCHAR(128)
+CREATE PROCEDURE dbo.buscarColumnas
+	@nombreColumna VARCHAR(MAX) = ''
+	,@nombreTabla VARCHAR(MAX) = ''
+    --> USO es EXEC dbo.buscarColumnas 'ind_movto','470'
 AS
-BEGIN
-    SELECT 
-        SCHEMA_NAME(t.schema_id) AS SchemaName,
-        t.name AS TableName,
-        c.name AS ColumnName,
-        ty.name AS DataType,
-        c.max_length AS MaxLength,
-        c.is_nullable AS IsNullable,
-        CASE 
-            WHEN pk.COLUMN_NAME IS NOT NULL THEN 'PK'
-            WHEN fk.COLUMN_NAME IS NOT NULL THEN 'FK'
-            ELSE NULL
-        END AS KeyType,
-        OBJECT_NAME(fk.referenced_object_id) AS ReferencedTable
-    FROM 
-        sys.tables t
-        INNER JOIN sys.columns c ON t.object_id = c.object_id
-        INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
-        LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE pk ON 
-            t.name = pk.TABLE_NAME AND 
-            c.name = pk.COLUMN_NAME AND
-            pk.CONSTRAINT_NAME LIKE 'PK%'
-        LEFT JOIN sys.foreign_key_columns fk ON 
-            t.object_id = fk.parent_object_id AND
-            c.column_id = fk.parent_column_id
-    WHERE 
-        c.name LIKE '%' + @ColumnName + '%'
-    ORDER BY 
-        SchemaName, TableName;
-END;
+	BEGIN
+		SET NOCOUNT ON;
+
+		DECLARE @consulta VARCHAR(MAX) = ''
+		SET @consulta = '
+
+		SELECT 
+			SCHEMA_NAME(t.schema_id) AS SchemaName,
+			t.name AS TableName,
+			c.name AS ColumnName,
+			ty.name AS DataType,
+			c.max_length AS MaxLength,
+			c.is_nullable AS IsNullable,
+			CASE 
+				WHEN EXISTS (
+					SELECT 1 FROM sys.index_columns ic 
+					JOIN sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id
+					WHERE ic.object_id = t.object_id AND ic.column_id = c.column_id 
+					AND i.is_primary_key = 1
+				) THEN ''PK''
+				WHEN fk.parent_column_id IS NOT NULL THEN ''FK''
+				ELSE NULL
+			END AS KeyType,
+			OBJECT_NAME(fk.referenced_object_id) AS ReferencedTable
+		FROM 
+			sys.tables t
+			INNER JOIN sys.columns c ON t.object_id = c.object_id
+			INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
+			LEFT JOIN sys.foreign_key_columns fk ON 
+				t.object_id = fk.parent_object_id AND
+				c.column_id = fk.parent_column_id
+		WHERE 
+			c.name LIKE ''%'+@nombreColumna+'%'+'''
+			'+CASE WHEN @nombreTabla = '' THEN '' ELSE 'and t.name like ''%'+@nombreTabla+'%'' ' END +'
+		ORDER BY 
+			SchemaName, TableName;
+		'
+
+		PRINT(@consulta)
+		EXEC(@consulta)
+
+	END;
